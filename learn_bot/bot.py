31278f098105ddaeea8a57e5606e7dce4ebaf534
Viktor Kertanov <thelonius19@gmail.com>
2022-12-16 13:40:54 +0300

    Conversation handler implemented. Now we can create questionnaires with a complex logic and fallbacks and store it in user_data

--- learn_bot/bot.py
+++ learn_bot/bot.py
@@ -1,17 +1,35 @@
 from config import logging
-from emoji import emojize
+from learn_bot.anketa import anketa_start, anketa_name, anketa_rating, anketa_skip, anketa_comment, anketa_fallback
 from learn_bot.handlers import\
     greet_user, guess_number_handler, send_flag_picture,\
     user_coordinates, talk_to_me, check_user_photo
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from config import API_TOKEN
 
 
 def main():
     echo_bot = Updater(API_TOKEN, use_context=True)
     
-    #dispatcher & registering handlers
     dp = echo_bot.dispatcher
+
+    anketa = ConversationHandler(
+        entry_points=[
+            MessageHandler(Filters.regex("^(Заполнить анкету)$"), anketa_start)
+        ],
+        states={
+            "name": [MessageHandler(Filters.text, anketa_name)],
+            "rating": [MessageHandler(Filters.regex("^(1|2|3|4|5)$"), anketa_rating)],
+            "comment": [
+                CommandHandler('skip', anketa_skip),
+                MessageHandler(Filters.text, anketa_comment)
+            ]
+        },
+        fallbacks=[
+            MessageHandler(Filters.text|Filters.photo|Filters.video|Filters.document|Filters.location, anketa_fallback)
+        ]
+    )
+
+    dp.add_handler(anketa)
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("guess", guess_number_handler))
     dp.add_handler(CommandHandler("flag", send_flag_picture))
--- learn_bot/bot_keyboard.py
+++ learn_bot/bot_keyboard.py
@@ -2,7 +2,11 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 
 def main_keyboard():
-    return ReplyKeyboardMarkup([['Прислать флаг', KeyboardButton('Мой координаты', request_location=True)]])
+    return ReplyKeyboardMarkup(
+        [
+            ['Прислать флаг', KeyboardButton('Мой координаты', request_location=True), 'Заполнить анкету']
+        ]
+    )
 
 if __name__ == '__main__':
 
